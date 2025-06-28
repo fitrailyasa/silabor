43823c46fa8153d881ccaf34916ab0626f68335a
Fitra Ilyasa <85192292+fitrailyasa@users.noreply.github.com>
2025-06-29 01:19:58 +0700

    Update upload.blade.php

--- resources/views/client/pengajuan-peminjaman/upload.blade.php
+++ resources/views/client/pengajuan-peminjaman/upload.blade.php
@@ -16,7 +16,6 @@
                 <th rowspan="2">Durasi Kegiatan</th>
                 <th rowspan="2">Template Surat</th>
                 <th rowspan="2">Status Validasi</th>
-                <th rowspan="2">Status Kegiatan</th>
                 <th rowspan="2">Catatan Admin</th>
                 <th rowspan="2">Aksi</th>
                 <th rowspan="2">Surat</th>
@@ -68,18 +67,6 @@
                             <span class="badge badge-warning">Menunggu</span>
                         @elseif ($laporan->status_validasi == 'Ditolak')
                             <span class="badge badge-danger">Ditolak</span>
-                        @endif
-                    </td>
-                    <td>
-                        @php
-                            $today = \Carbon\Carbon::today();
-                            $tglPengembalian = \Carbon\Carbon::parse($laporan->tgl_pengembalian);
-                        @endphp
-
-                        @if ($today->gt($tglPengembalian))
-                            <span class="badge badge-success">Selesai</span>
-                        @else
-                            <span class="badge badge-warning">Sedang Berjalan</span>
                         @endif
                     </td>
                     <td>{{ $laporan->catatan ?? '-' }}</td>
